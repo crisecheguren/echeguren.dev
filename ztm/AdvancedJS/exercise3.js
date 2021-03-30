a5482d5aa25f9d37759051564b41a7db2de81ada
Cris <crisecheguren@gmail.com>
2021-03-29 20:08:48 -0400

    exercises

---
+++ ztm/AdvancedJS/exercise3.js
@@ -0,0 +1,72 @@
+// change everything below to the newer Javascript!
+
+// let + const
+let a = 'test';
+const b = true;
+const c = 789;
+a = 'test2';
+
+
+// Destructuring
+const person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+var firstName = person.firstName;
+var lastName = person.lastName;
+var age = person.age;
+var eyeColor = person.eyeColor;
+
+const {firstName, lastName, age, eyeColor} = person;
+
+
+// Object properties
+
+const a = 'test';
+const b = true;
+const c = 789;
+
+const okObj = { a, b, c };
+
+
+// Template strings
+var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+
+const message2 = `Hello ${firstName} have I met you before? I think we met in ${city} last summer, no???!?`;
+
+
+// default arguments
+// default age to 10;
+function isValidAge(age) {
+    return age
+}
+
+function isValidAge2(age="32") {
+  return age;
+}
+
+// Symbol
+// Create a symbol: "This is my first Symbol"
+
+// Arrow functions
+function whereAmI(username, location) {
+    if (username && location) {
+        return "I am not lost";
+    } else {
+        return "I am totally lost!";
+    }
+}
+
+const whereAmI2 = (username, location) => {
+  if (username && location) {
+        return "I am not lost";
+    } else {
+        return "I am totally lost!";
+    } 
+}
+
+let whereAmI3; 
+whereAmI3 => (username, location) ? "I am not lost" : "I am totally lost";
